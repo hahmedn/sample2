ec8487230ef74123162f3b4077b5c94bb87e46a6
Ahmed N <hasib.ahmed-n@capgemini.com>
2018-11-15 11:46:33 +0530

    Hello2 commited for 1st time

---
+++ Hello2.java
@@ -0,0 +1,14 @@
+package com.capgemini.project.MavenExample2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Everyone!" );
+		System.out.println( "Bad Morning" );
+    }
+}
